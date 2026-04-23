823452d659192578701b46c813d9e7f3982dd8a9
PrzemCza <167012710+PrzemCza@users.noreply.github.com>
2026-04-23 11:22:22 +0200

    Add assetPrefix to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   basePath: "/zagle",
-  output: "standalone",
+  assetPrefix: "/zagle",
 };
 
 export default nextConfig;
